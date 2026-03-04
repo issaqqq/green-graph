2e6fa105def5304abe19f438e0384e992a130a3f
Issaq <kadavathepeedikayil786786@gmail.com>
2026-03-04 13:41:28 +0530

    .

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = "./data.json";
 const markCommit = (x, y) => {
     const date = moment()
     .subtract(0, "y")
-    .subtract(7, "d")
+    .add(-8, "d")
     .add(x, "w")
     .add(y, "d")
     .format();
