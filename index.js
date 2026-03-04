6ec57047412359413145453e06b160aecb205957
Issaq <kadavathepeedikayil786786@gmail.com>
2026-03-04 13:53:27 +0530

    add

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = "./data.json";
 const markCommit = (x, y) => {
     const date = moment()
     .subtract(0, "y")
-    .subtract(4, "d")
+    .subtract(8, "d")
     .add(x, "w")
     .add(y, "d")
     .format();
